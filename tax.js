ea1beb81a5f665afbd7419025b58630a51971d83
SMMC-Web-Design <alan.burt@mackillop.act.edu.au>
2016-05-29 10:49:44 +1000

    basic tax files.

---
+++ tax.js
@@ -0,0 +1,6 @@
+function changeStuff() {
+    var inputIncomeElement = document.getElementById("inputIncome");
+    var name = inputIncomeElement.value;
+    var titleElement = document.getElementById("");
+    Element.innerHTML = ;
+}
