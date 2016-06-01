0695a66cc3dbc27e291bb105c61ea9bfb7c6125a
SMMC-Web-Design <alan.burt@mackillop.act.edu.au>
2016-06-01 13:23:12 +1000

    almost finished files
    graph and info about graph are all that it left

--- tax.js
+++ tax.js
@@ -1,6 +1,23 @@
+function calculateTax(income) {
+    var tax;
+    if (income < 16000) {
+        tax = 0;
+    } else if (income < 52500 ) {
+        tax = (income - 16000) *0.1;
+    } else if (income < 113000){
+        tax = (income - 52500) *0.16 + 3650; 
+    } else if (income < 184500){
+        tax = (income - 113000) *0.24 +13330;
+    } else {
+        tax = (income - 184500) * 0.36 + 30490;
+    }
+       
+    return tax;
+}
+
 function changeStuff() {
-    var inputIncomeElement = document.getElementById("inputIncome");
-    var name = inputIncomeElement.value;
-    var titleElement = document.getElementById("");
-    Element.innerHTML = ;
+    var input = document.getElementById("box");
+    var number = input.value;
+    var output = document.getElementById("output");
+    output.value = calculateTax(number);
 }
