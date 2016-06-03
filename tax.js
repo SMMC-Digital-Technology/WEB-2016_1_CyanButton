c580d9ccd8b4d288fb0b2fd07d7e917b5a30d2d4
SMMC-Web-Design <alan.burt@mackillop.act.edu.au>
2016-06-03 12:35:06 +1000

    Final tax files
    fully completed tax files ;)

--- tax.js
+++ tax.js
@@ -1,4 +1,4 @@
-function changeStuff (income) {
+function incomeTax (income) {
     var tax;
     if (income < 16000) {
         tax = 0;
@@ -15,17 +15,10 @@ function changeStuff (income) {
     return tax;
 }
 
-/**
- * Tutorial on making graphs with plotly.js
- **/
-
-/**
- * This function calculates the interest owed on a simple interest loan.
- * Parameters: principle, rate (per year) and time (in years).
- * Returns: the simple interest
- */
-function simpleInterest(principle, rate, time) {
-    return principle * rate * time;
+function changeStuff() {
+    var input = document.getElementById("box");
+    var output = document.getElementById("output");
+    output.value = incomeTax(input.value);
 }
 
 /**
@@ -34,22 +27,22 @@ function simpleInterest(principle, rate, time) {
 function drawGraph() {
 
     // declare all variables (variables inside a function are created when the function is called)
-    var trace, data, layout, time, interest;
+    var trace, data, layout, income, tax;
     
-    time = [];
-    interest = []; 
+    income = [];
+    tax = []; 
     
 //generate the values 
- for (var i = 0; i < 26; i++) {
-     time[i] = i; 
-     interest[i] = simpleInterest(100, 0.5, i); 
+ for (var i = 0; i < (250000/500); i++) {
+     income[i] = i*500;
+     tax[i] = incomeTax(income[i]); 
         
     }
 
     /* Trace is an object that stores the x and y values. The x, y and type variables are required mandatory. */
     trace = {
-        x: time, // xvalues
-        y: interest, // y values
+        x: income, // xvalues
+        y: tax, // y values
         type: 'scatter', // the type of graph
         mode: 'lines+markers', // how the data should be displayed
         name: "trace" // a name for the data to appear on the legend (if present)
@@ -60,13 +53,13 @@ function drawGraph() {
 
     /* To add a title and axis labels, we need to create a layout (another object). Setting a layout is optional, but they do improve the graph. */
     layout = {
-        title: "A simple graph in Plotly",
+        title: "Income amount and the given Tax",
         yaxis: {
             title: "Income", // give the axis a label
             zerolinewidth: 1.5 // makes the zero line thicker
         },
         xaxis: {
-            title: "Time",
+            title: "Tax",
             zerolinewidth: 1.5,
            //range; [0,5] //sets a minimum and maximum value for the axis
         }
